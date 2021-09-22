7fc59c32d6dca8fc74546bba748346e79da6d21d
Ray He <devrayhe@gmail.com>
2021-09-22 15:26:08 +1000

    fix the bug where line through was saved automatically, code refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
-// Bootstrap to update main layout of the page 
 
 var timeTableDisplayEl = $("#timeTableDisplay");
 var dateDisplayEl = $("#currentDay");
 
 function init() {
 
-  // For loop generate table row x 8, assgin relevant class and IDs to each.
-  // Jquery to dynamically update html and css
+  // Generate table row x 8, assgin relevant class and IDs to each.
+  createTimeBlocks();
+  displayDate();
+
+  // Calling these 3 functions after all page elements created to display todays date, update Schedule on the hour and set the time block color update on a 3 seconds timer.
+  clickEventListener();
+  updateSchedule();
+  updateTimer();
+}
+
+// Generate table row x 8, assgin relevant class and IDs to each.
+function createTimeBlocks() {
+
   for (i = 9; i < 18; i++ ) {
     var hour = moment(i,"H").format("hA");
-    
     var timeTableRowEl = $("<tr>").addClass("row");
     var formRowEl = $("<form>");
 
     var hourEl = $("<td>")
       .addClass("col-2 hour")
-      .attr({id:hour})
+      .attr("id","hour")
       .text(hour);
     
     var inputRowEl = $("<td>")
@@ -34,6 +43,7 @@ function init() {
       class:"time-block"})
       .appendTo(formRowEl);
         
+    // Assign each input button a unique ID
     $("<input>").attr({
       type:"button",
       id: hour + "Btn"})
@@ -51,13 +61,22 @@ function init() {
 
     timeTableDisplayEl.append(timeTableRowEl);
   }
+}
 
-  // Add event listener to the timeblocks, based on target update relevant row only.
-  $(":button").click(function (event) {
+// Moment.js to display current date and time
+// handle displaying the date
+function displayDate() {
+  var dateToday = moment().format("dddd, MMMM Do");
+  dateDisplayEl.text(dateToday);
+}
+
+// Add event listener to button and timeblocks data
+function clickEventListener() {
+   // Add event listener to the timeblocks, based on target update relevant row only.
+   $(":button").click(function (event) {
 
     // Dom traverse to select the textarea of the save button
     let targetInput = $(event.target).parent().prev().children().children()[0];
-    // let userInput = targetInput.value.trim();
     let targetInputId = targetInput.id;
 
     var userInput = {
@@ -87,23 +106,12 @@ function init() {
   // Add event listener to the timeblocks, double click to place line through text to indicate it's done.
   $(".time-block").dblclick(function (event) {
     let targetEl = event.target;
-    // line through only applicable on time block which has events.
+
+    // line through only applicable on time block which has data on it.
     if (JPLSGI(targetEl.id).content) {
       $(targetEl).toggleClass("line-through");
-      
-      // Get data from local storage and update line through status
-      let targetElData = JPLSGI(targetEl.id);
-      targetElData.lineThrough = $(targetEl).hasClass("line-through");
-      localStorage.setItem(targetEl.id, JSON.stringify(targetElData));
     }
-    
-
   })
-
-  // Calling these 3 functions after all page elements created to display todays date, update Schedule on the hour and set the time block color update on a 3 seconds timer.
-  displayDate();
-  updateSchedule();
-  updateTimer();
 }
 
 // Update schedule by getting data from local storage
@@ -127,40 +135,17 @@ function updateSchedule() {
         }
       }
     }
-
-    // textareaData[i] = {
-    //   id:"",
-    //   content:"",
-    //   style:""
-    // };
-
-    // textareaData[i].id = textareaEl[i].id;
-
-    // Get data from localStorage
-    // if (localStorage.getItem(textareaData[i].id)) {
-    //   textareaData[i].content = JSON.parse(localStorage.getItem(textareaData[i].id)).content;
-
-    //   // Display content to the time block if data is retrieved from local storage
-    //   if (textareaData[i].content) {
-    //     $("#" + textareaData[i].id).text(textareaData[i].content);
-    //   }
-    // }
-
-    // console.log(localStorage.getItem(textareaData[i].id));
-    // console.log(JSON.parse(localStorage.getItem(textareaData[i].id)));
   }
-
-  // Loop through data from localstorage, display data on relevant time block
-  // for (let j=0; j<textareaData.length; j++) {
-  //   if (textareaData[j].content) {
-  //     $("#" + textareaData[j].id).text(textareaData[j].content);
-  //   }
-  // }
 }
 
-// Define a function to perform JSON.parse(localStorage.getItem()) and return the value.
-function JPLSGI (para) {
-  return JSON.parse(localStorage.getItem(para));
+// Timer function to update time block color every 3 seconds
+function updateTimer() {
+  
+  updateTimeblockColor();
+
+  timer = setInterval(function() {
+    updateTimeblockColor();
+  }, 3000);
 }
 
 // With Moment.js .diff() method, compare timeblock vs current time, update attributes accordingly.
@@ -194,26 +179,11 @@ function updateTimeblockColor() {
       $("#" + allHourEl[i].id ).next().toggleClass("future", true);
     }
   }
-
-  // var hourEl3 = $("#timeTableDisplay").find(".hour");
-  // console.log(hourEl3);
 }
 
-// Timer function to update time block color every 3 seconds
-function updateTimer() {
-  
-  updateTimeblockColor();
-
-  timer = setInterval(function() {
-    updateTimeblockColor();
-  }, 3000);
-}
-
-// Moment.js to display current date and time
-// handle displaying the date
-function displayDate() {
-  var dateToday = moment().format("dddd, MMMM Do");
-  dateDisplayEl.text(dateToday);
+// Define a function to perform JSON.parse(localStorage.getItem()) and return the value.
+function JPLSGI (para) {
+  return JSON.parse(localStorage.getItem(para));
 }
 
 init();
